6d53b867519b6464d44c74a69832a6df4a7a4801
Kris Frey <kfrey@mit.edu>
2016-04-13 11:47:57 -0400

    Reduced octomap cloud in queue size to 1 -- we always want to use most recent cloud

--- octomap_server/src/OctomapServer.cpp
+++ octomap_server/src/OctomapServer.cpp
@@ -238,7 +238,7 @@ OctomapServer::OctomapServer(ros::NodeHandle private_nh_)
     {
       stream_name += boost::to_string(i);
     }
-    m_pointCloudSubVec.push_back(new message_filters::Subscriber<sensor_msgs::PointCloud2> (m_nh, stream_name, 5));
+    m_pointCloudSubVec.push_back(new message_filters::Subscriber<sensor_msgs::PointCloud2> (m_nh, stream_name, 1));
     m_tfPointCloudSubVec.push_back(new tf::MessageFilter<sensor_msgs::PointCloud2> (*m_pointCloudSubVec[i], m_tfListener, m_worldFrameId, 5));
     m_tfPointCloudSubVec[i]->registerCallback(boost::bind(&OctomapServer::insertCloudCallback, this, _1, i));   // bind source id i to callback
   }
